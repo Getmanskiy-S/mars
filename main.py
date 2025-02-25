4ee957a0311d156db1ff950294475db57b317b9d
student <seva-get@bk,ru>
2025-02-25 18:08:48 +0300

    "Список профессий"

--- main.py
+++ main.py
@@ -8,9 +8,33 @@ app = Flask(__name__)
 def index(title):
     return render_template('base.html', title=title)
 
+
 @app.route('/training/<prof>')
 def training(prof):
     return render_template('training.html', prof=prof.lower())
+
+
+@app.route('/list_prof/<lst>')
+def list_prof(lst):
+    professions = [
+        'Астронавт',
+        'Астрофизик',
+        'Космический инженер',
+        'Космический биолог',
+        'Космогеолог',
+        'Специалист по управлению полетами',
+        'Разработчик ракетных двигателей',
+        'Специалист по спутниковой связи',
+        'Космический архитектор',
+        'Космический врач',
+        'Специалист по космическому праву',
+        'Оператор телескопа',
+        'Планетолог',
+        'Разработчик скафандров',
+        'Техник по обслуживанию космической техники',
+        'Специалист по радиационной безопасности в космосе'
+    ]
+    return render_template('list_prof.html', lst=lst, professions=professions)
 
 
 if __name__ == '__main__':
